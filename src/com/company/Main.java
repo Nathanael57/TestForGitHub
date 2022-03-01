82e1435d6f20abcfee43fc3df561bae0c84e4aff
Nathanael57 <ndabi1@stu.vistula.edu.pl>
2022-03-01 05:49:27 +0100

    Commit 2

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello Vistula - My Name is Nathanael");
+
+        System.out.println("Hello Vistula - My first name is Nathanael and my last name is Dabi");
+
+
+    }
+}
